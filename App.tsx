804aa3f9fccc1b53ac9f0f0d2585810f28b7e8a2
fanenunes-web <fanenunes@hotmail.com>
2025-11-27 03:28:04 -0300

    Update import path for Navigation component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
-import Navigation from './components/Navigation';
+import Navigation from './components/Navigation/Navigation';
 import Hero from './components/Hero';
 import VideoFeed from './components/VideoFeed';
 import Columnists from './components/Columnists';
